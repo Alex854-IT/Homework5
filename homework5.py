e300adb8c92aa5f7982597eb641d412730cfecee
Alex854-IT <lv854@rambler.ru>
2024-06-28 22:30:15 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 3, False, "Список")
+print(immutable_var)
+mutable_list = [10, 20, True, "Перечень"]
+print(mutable_list)
+mutable_list [1] = 30
+mutable_list [2] = False
+print (mutable_list)
